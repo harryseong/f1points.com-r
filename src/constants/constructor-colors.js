30f83e48b36b0243e8866601c6a139464aae11c1
Harry Seong <harry@northwestern.edu>
2019-06-17 08:48:20 -0500

    Corrected team colors

--- src/constants/constructor-colors.js
+++ src/constants/constructor-colors.js
@@ -1,12 +1,12 @@
 export const constructorColors = {
-  mercedes: { color: "#00D2BE", color2: "#00d2bd55" },
+  mercedes: { color: "#00D2BE", color2: "#00d2be55" },
   ferrari: { color: "#dc0000", color2: "#dc000055" },
   red_bull: { color: "#1e41ff", color2: "#1e41ff55" },
   mclaren: { color: "#ff8700", color2: "#ff870055" },
   renault: { color: "#fff500", color2: "#fff50055" },
-  racing_point: { color: "#9b0000", color2: "#9b000055" },
+  racing_point: { color: "#F596C8", color2: "#F596C855" },
   toro_rosso: { color: "#469bff", color2: "#469bff55" },
   haas: { color: "#f0d787", color2: "#f0d78755" },
-  alfa: { color: "#00d2be", color2: "#00d2be55" },
+  alfa: { color: "#9B0000", color2: "#9B000055" },
   williams: { color: "#ffffff", color2: "#ffffff55" }
 };
